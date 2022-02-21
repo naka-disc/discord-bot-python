c1ac40f3832bfc1084724894860bb5613e53a74a
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-02-22 00:16:46 +0900

    オートインクリメント忘れ

--- resources/Init.sql
+++ resources/Init.sql
@@ -1,12 +1,11 @@
 -- VCの入退室ログ
 DROP TABLE vc_access_records;
 CREATE TABLE vc_access_records (
-    id INT    ,                 -- サロゲートキー
-    member_id TEXT,             -- Discord Member ID
-    member_name TEXT,           -- Discord Member Name
-    member_discriminator TEXT,  -- Discord Member Discriminator #0000 みたいなやつ
-    in_datetime TEXT,           -- 入室日時
-    out_datetime TEXT,          -- 退室日時
-    stay_second INT,            -- 滞在時間
-    PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,   -- サロゲートキー
+    member_id TEXT,                         -- Discord Member ID
+    member_name TEXT,                       -- Discord Member Name
+    member_discriminator TEXT,              -- Discord Member Discriminator #0000 みたいなやつ
+    in_datetime TEXT,                       -- 入室日時
+    out_datetime TEXT,                      -- 退室日時
+    stay_second INT                         -- 滞在時間
 );
